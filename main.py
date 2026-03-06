324683f0d56bcd6f924d215a7593e403841cb419
ArteProd <100274091+ArteProd@users.noreply.github.com>
2026-03-06 15:36:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3586,7 +3586,7 @@ if (updatedUser.avatar) {
                 }
             }
 
-            /*let statusInterval = null;
+            let statusInterval = null;
 
             function startStatusInterval(userId) {
                 if (statusInterval) clearInterval(statusInterval);
@@ -3604,7 +3604,7 @@ if (updatedUser.avatar) {
                     clearInterval(statusInterval);
                     statusInterval = null;
                 }
-            }*/
+            }
 
             function updateHeaderStatus(user) {
                 const container = document.getElementById('chatStatus');
